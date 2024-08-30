3715f7ad597370c9cb247d51301d500a061dc5fc
daniel sonbolian <dsal3389@gmail.com>
2024-08-31 01:21:32 +0300

    script and dark theme

---
+++ main.js
@@ -0,0 +1,52 @@
+// type alias sleep so we can actually sleep in stupid javascript
+async function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function typeWriterAnimation(element, text) {
+    let i = 0;
+
+    // remove characters
+    while (element.innerText) {
+        element.innerText = element.innerText.slice(0, -1);
+        await sleep(150);
+    }
+
+    // add characters
+    while (i < text.length) {
+        element.textContent += text.charAt(i++);
+        await sleep(50);
+    }
+}
+
+async function cycleTitles() {
+    const element = document.getElementById("title");
+    const titles = [
+        "Software developer", 
+        "Full stack", 
+        "Network engineer", 
+        "Python developer", 
+        "Rust developer", 
+        "C developer"
+    ];
+    let i = 0;
+
+    while (true) {
+        if (i >= titles.length) {
+            i = 0;
+        }
+
+        let value = titles[i++];
+        await typeWriterAnimation(element, value);
+        await sleep(1500);
+    }
+}
+
+(async function() {
+    cycleTitles();
+
+    const responses = await Promise.all([fetch("./data/left-pane.json"), fetch("./data/right-pane.json")]);
+    const [left, right] = await Promise.all([responses[0].json(), responses[1].json()]);
+
+    typeWriterAnimation(document.getElementById("description"), left.description);
+})();
