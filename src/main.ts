85bc3541941972ded3a4adaa89dd5ce2fd85c3d5
daniel sonbolian <dsal3389@gmail.com>
2025-01-01 22:56:55 +0200

    refacto site from scratch

--- src/main.ts
+++ src/main.ts
@@ -1,86 +1,37 @@
-import ScrollMagic from "scrollmagic";
+import './style.css'
 import anime from "animejs";
+import Scrollmagic from "scrollmagic";
 
-function sleep(ms: number): Promise<void> {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
-
-async function typeWriter(element: HTMLParagraphElement | HTMLSpanElement, strings: String[]) {
-  let index = 0;
-
-  while (true) {
-    while (element.innerText) {
-      element.innerText = element.innerText.slice(0, -1);
-      await sleep(170);
-    }
-
-    for (let c of strings[index++]) {
-      element.textContent += c;
-      await sleep(100);
-    }
-
-    await sleep(1000);
-    if (index >= strings.length) {
-      index = 0;
-    } 
-  }
-}
-
-// animations ---
-
-typeWriter(
-    document.getElementById("scene-1-titles")!,
-    [
-        "Python developer",
-        "C developer",
-        "Rust developer",
-        "full stack developer"
-    ]
-);
 
 anime({
-  targets: "#scene-1-img",
+  targets: '#intro-pfp',
   keyframes: [
     { translateY: -25 },
     { translateY: 0 }
   ],
   easing: 'easeInOutQuad',
   duration: 5500,
-  loop: true
+  loop: true,
 });
 
 let skillAnimation = anime({
-  targets: ".scene-3-skill",
+  targets: ".skill-card",
+  opacity: [0, 1],
   translateX: (_: HTMLDivElement, i: number): number[] => {
     return i % 2 === 0 ? [-200, 0] : [200, 0];
   },
-  opacity: [0, 1],
-  duration: 5500,
   delay: anime.stagger(1500),
+  duration: 2000,
   autoplay: false,
 });
 
-// define scroll margic scenes ---
-
-let controller = new ScrollMagic.Controller();
-
-// scene 2
-new ScrollMagic.Scene({
-  triggerElement: "#scene-2",
-  triggerHook: "onLeave",
-  duration: 400,
-})
-.setPin("#scene-2-info")
-.addTo(controller);
-
-// scene 3
-new ScrollMagic.Scene({
-  triggerElement: "#scene-3",
-  offset: 400,
-  duration: 1500
-})
-.on<ScrollMagic.ProgressEvent>("progress", (event) => {
-  skillAnimation.seek(skillAnimation.duration * event.progress!);
-})
-.setPin("#scene-3-skills")
-.addTo(controller);
+new Scrollmagic.Controller()
+  .addScene(
+    new Scrollmagic.Scene({
+      triggerElement: "#skills-section",
+      duration: 1000,
+      offset: -700,
+    }).on<Scrollmagic.ProgressEvent>("progress", (event) => {
+      skillAnimation.seek(skillAnimation.duration * event.progress!);
+    })
+  );
