3ff35c004a9c78ac78b847df1b5320fb7ff68e6f
daniel sonbolian <dsal3389@gmail.com>
2025-01-11 21:46:11 +0200

    added more images

--- src/main.ts
+++ src/main.ts
@@ -20,8 +20,16 @@ let skillAnimation = anime({
   translateX: (_: HTMLDivElement, i: number): number[] => {
     return i % 2 === 0 ? [-200, 0] : [200, 0];
   },
-  delay: anime.stagger(1500),
+  delay: anime.stagger(2000),
   duration: 2000,
+  autoplay: false,
+});
+
+let booksAnimation = anime({
+  targets: ".book",
+  opacity: [0, 1],
+  scale: [1.1, 1],
+  delay: anime.stagger(1000),
   autoplay: false,
 });
 
@@ -30,8 +38,16 @@ new Scrollmagic.Controller()
     new Scrollmagic.Scene({
       triggerElement: "#skills-section",
       duration: 1000,
-      offset: -700,
+      offset: -500,
     }).on<Scrollmagic.ProgressEvent>("progress", (event) => {
       skillAnimation.seek(skillAnimation.duration * event.progress!);
+    })
+  ).addScene(
+    new Scrollmagic.Scene({
+      triggerElement: "#books-section",
+      duration: 1500,
+      offset: -300,
+    }).on<Scrollmagic.ProgressEvent>("progress", (event) => {
+      booksAnimation.seek(booksAnimation.duration * event.progress!);
     })
   );
